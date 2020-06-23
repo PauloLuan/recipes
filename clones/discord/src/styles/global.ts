bccfeffd65653e56be8eb59b55307311cbf7744e
Paulo Luan <pauloluan.inova@gmail.com>
2020-06-23 05:00:56 -0300

    👌 IMPROVE: discord: added base colors

--- clones/discord/src/styles/global.ts
+++ clones/discord/src/styles/global.ts
@@ -16,4 +16,24 @@ export default createGlobalStyle`
     outline: 0;
     font-family: 'Roboto', sans-serif;
   }
+
+  :root {
+    --primary: #36393f;
+    --secondary: #2f3136;
+    --tertiary: rgb(32,34,37);
+    --quaternary: #292b2f;
+    --quinary: #393d42;
+    --senary: #828386;
+    
+    --white: #fff;
+    --gray: #8a8c90;
+    --chat-input: rgb(64,68,75);
+    --symbol: #74777a;
+    --notification: #f84a4b;
+    --discord: #6e86d6;
+    --mention-detail: #f9a839;
+    --mention-message: #413f3f;
+    --link: #5d80d6;
+    --reativa: #6633cc;
+  }
 `
