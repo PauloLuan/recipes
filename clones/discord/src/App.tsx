5a6cfdd80cfb616011caa6661b8e44b3894e19f4
Paulo Luan <pauloluan.inova@gmail.com>
2020-06-23 04:45:32 -0300

    📦 NEW: discord: added global styles

--- clones/discord/src/App.tsx
+++ clones/discord/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import GlobalStyles from './styles/global'
 
 function App () {
   return (
-    <div>
-      <p>Hello Discord</p>
-    </div>
+    <>
+      <h1>Hello Discord</h1>
+      <GlobalStyles />
+    </>
   )
 }
 
