6e50add639783b562ce1d5bdd4f86b42115bc84f
Paulo Luan <pauloluan.inova@gmail.com>
2020-06-23 19:51:04 -0300

    📦 NEW: create ServerName component

---
+++ clones/discord/src/components/ServerName/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+const ServerName: React.FC = () => {
+  return <Container></Container>
+}
+
+export default ServerName
---
+++ clones/discord/src/components/ServerName/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  grid-area: SERVER_NAME;
+  background: red;
+`
