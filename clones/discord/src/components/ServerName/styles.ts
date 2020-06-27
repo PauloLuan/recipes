44e124ca8b50ecf38e389b4b573108440a83f75b
Paulo Luan <pauloluan.inova@gmail.com>
2020-06-27 05:32:27 -0300

    📦 NEW: ServerName: improved padding and color

--- clones/discord/src/components/ServerName/styles.ts
+++ clones/discord/src/components/ServerName/styles.ts
@@ -5,8 +5,9 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 10px;
-  background: black;
+  padding: 0 11px 0 16px;
+
+  background-color: var(--secondary);
 `
 
 export const Title = styled.div`
