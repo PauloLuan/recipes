20eb76d400209acfe2a6ef6348e46362eb38c8a5
Paulo Luan <pauloluan.inova@gmail.com>
2020-06-27 06:18:39 -0300

    📦 NEW: ChannelList: added initial structure

--- clones/discord/src/components/ChannelList/index.tsx
+++ clones/discord/src/components/ChannelList/index.tsx
@@ -1,9 +1,61 @@
 import React from 'react'
 
-import { Container } from './styles'
+import {
+  Container,
+  ListItem,
+  ChannelText,
+  AddIcon,
+  Info,
+  HashtagIcon,
+  ChannelName,
+  IconsContainer,
+  AddPersonIcon,
+  SettingsIcon
+} from './styles'
 
 const ChannelList: React.FC = () => {
-  return <Container></Container>
+  return (
+    <Container>
+      <ListItem>
+        <ChannelText>Canais de Texto</ChannelText>
+        <AddIcon />
+      </ListItem>
+      <ListItem>
+        <Info>
+          <HashtagIcon />
+          <ChannelName>porrada-livre</ChannelName>
+        </Info>
+        <IconsContainer>
+          <AddPersonIcon />
+          <SettingsIcon />
+        </IconsContainer>
+      </ListItem>
+      <ListItem>
+        <Info>
+          <HashtagIcon />
+          <ChannelName>treta de mulher</ChannelName>
+        </Info>
+      </ListItem>
+      <ListItem>
+        <Info>
+          <HashtagIcon />
+          <ChannelName>porradaria em grupo</ChannelName>
+        </Info>
+      </ListItem>
+      <ListItem>
+        <Info>
+          <HashtagIcon />
+          <ChannelName>só tapão de qualidade</ChannelName>
+        </Info>
+      </ListItem>
+      <ListItem>
+        <Info>
+          <HashtagIcon />
+          <ChannelName>mindingo saindo no pau</ChannelName>
+        </Info>
+      </ListItem>
+    </Container>
+  )
 }
 
 export default ChannelList
--- clones/discord/src/components/ChannelList/styles.ts
+++ clones/discord/src/components/ChannelList/styles.ts
@@ -2,5 +2,23 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   grid-area: CHANNEL_LIST;
-  background: blue;
+  background: var(--secondary);
 `
+
+export const ListItem = styled.div``
+
+export const ChannelText = styled.div``
+
+export const AddIcon = styled.div``
+
+export const Info = styled.div``
+
+export const HashtagIcon = styled.div``
+
+export const ChannelName = styled.div``
+
+export const IconsContainer = styled.div``
+
+export const AddPersonIcon = styled.div``
+
+export const SettingsIcon = styled.div``
