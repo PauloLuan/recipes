1c32c8009872df925a07c6ffe6534b3f6c4a0a29
Paulo Luan <pauloluan.inova@gmail.com>
2020-06-29 19:09:37 -0300

    👌 IMPROVE: UserList: added custom scrollbar

--- clones/discord/src/components/UserList/styles.ts
+++ clones/discord/src/components/UserList/styles.ts
@@ -10,6 +10,17 @@ export const Container = styled.div`
 
   max-height: calc(100vh - 46px);
   overflow-y: scroll;
+
+  ::-webkit-scrollbar {
+    width: 4px;
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: var(--tertiary);
+    border-radius: 4px;
+  }
+  ::-webkit-scrollbar-track {
+    background-color: var(--secondary);
+  }
 `
 
 export const Status = styled.div`
