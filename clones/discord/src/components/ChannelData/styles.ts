0e4ddeed137fb0f70d4f65d0c05619acdc068234
Paulo Luan <pauloluan.inova@gmail.com>
2020-06-30 04:39:57 -0300

    👌 IMPROVE: MainMessages: added initial css for main container and input wrapper

--- clones/discord/src/components/ChannelData/styles.ts
+++ clones/discord/src/components/ChannelData/styles.ts
@@ -2,8 +2,20 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   grid-area: CHANNEL_DATA;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `
 
 export const MessagesContainer = styled.div``
-export const InputContainer = styled.div``
-export const Input = styled.input``
+
+export const InputContainer = styled.div`
+  width: 100%;
+  height: 86px;
+  background: blue;
+`
+
+export const Input = styled.input`
+  padding: 10px;
+`
