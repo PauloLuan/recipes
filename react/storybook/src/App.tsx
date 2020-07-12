0184c16030799414cc7c6bed16bc178e458b6dec
Paulo Luan <pauloluan.inova@gmail.com>
2020-07-12 07:25:57 -0300

    📦 NEW: added simple component example

--- react/storybook/src/App.tsx
+++ react/storybook/src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import './App.css'
+import { Example } from './Example'
 
 function App () {
-  return <div>Storybook example</div>
+  return <Example customText='Storybook example'></Example>
 }
 
 export default App
