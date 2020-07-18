9ac2fe2eb25f552c6d686d2b801a846fcfedddef
Paulo Luan <pauloluan.inova@gmail.com>
2020-07-18 05:43:22 -0300

    👌 IMPROVE: added initial tags

--- react/frontend-mentors/src/Challenges/four-card-feature/fourcard.component.tsx
+++ react/frontend-mentors/src/Challenges/four-card-feature/fourcard.component.tsx
@@ -1,5 +1,24 @@
 import React from 'react'
 
 export const FourCard: React.FC = () => {
-  return <div>FourCard</div>
+  return (
+    <>
+      <h1>Reliable, efficient delivery</h1>
+      <h1>Powered by Technology</h1>
+      <p>
+        Our Artificial Intelligence powered tools use millions of project data
+        points to ensure that your project is successful
+      </p>
+      <h2>Supervisor</h2>
+      <p>Monitors activity to identify project roadblocks</p>
+      <h2>Team Builder</h2>
+      <p>
+        Scans our talent network to create the optimal team for your project
+      </p>
+      <h2>Karma</h2>
+      <p>Regularly evaluates our talent to ensure quality</p>
+      <h2>Calculator</h2>
+      <p>Uses data from past projects to provide better delivery estimates</p>
+    </>
+  )
 }
