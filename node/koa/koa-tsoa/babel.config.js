180eb5bd762e43c15c53534f4e25b31592efa3cd
Paulo Luan <pauloluan.inova@gmail.com>
2020-07-29 05:13:35 -0300

    🐛 FIX: removed unused folders

--- node/koa/koa-tsoa/babel.config.js
+++ node/koa/koa-tsoa/babel.config.js
@@ -16,9 +16,7 @@ module.exports = {
       {
         alias: {
           '@config': './src/config',
-          '@models': './src/models',
-          '@controllers': './src/controllers',
-          '@views': './src/views'
+          '@src': './src'
         }
       }
     ]
