ebbd3b6684ab85b3ee8eb65904530048ff425e0b
Paulo Luan <pauloluan.inova@gmail.com>
2020-08-08 07:33:56 -0300

    👌 IMPROVE: added global root colors

--- react/clones/github/src/styles/GlobalStyles.ts
+++ react/clones/github/src/styles/GlobalStyles.ts
@@ -20,4 +20,31 @@ export const GlobalStyles = createGlobalStyle`
   ul {
     list-style: none;
   }
+
+  :root {
+    --primary: #fff;
+    --black: #1b1f23;
+    --gray: #586069;
+    --gray-light: #6a737d;
+    --gray-dark: #24292e;
+    --orange: #f9826c;
+    --logo: #fff;
+    --header: #24292e;
+    --username: #666;
+    --search: rgba(255; 255; 255; 0.13);
+    --search-placeholder: hsla(0;0%;100%;.75);
+    --icon: #6a737d;
+    --link: #0366d6;
+    --border: #e1e4e8;
+    --ticker: rgba(209;213;218;.5);
+    
+    --calendar-scale-0: #ebedf0;
+    --calendar-scale-1: #9BE9A8;
+    --calendar-scale-2: #3FC463;
+    --calendar-scale-3: #30A14E;
+    --calendar-scale-4: #216E3A;
+    --javascript: #f1e05a;
+    --typescript: #2b7489;
+    --other-language: #8257e5;
+  }
 `
