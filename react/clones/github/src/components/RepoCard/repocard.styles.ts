68f930684fff90863ab42ce9f5f2f1a954b20338
Paulo Luan <pauloluan.inova@gmail.com>
2020-08-16 04:14:34 -0300

    🔧 chore: added base icon css

--- react/clones/github/src/components/RepoCard/repocard.styles.ts
+++ react/clones/github/src/components/RepoCard/repocard.styles.ts
@@ -1,7 +1,14 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const Container = styled.div`
   border: 1px solid var(--border);
   border-radius: 6px;
   padding: 16px;
+`
+
+export const baseIconCSS = css`
+  width: 16px;
+  height: 16px;
+  flex-shrink: 0;
+  fill: var(--icon)
 `
