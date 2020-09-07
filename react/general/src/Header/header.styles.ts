a17368c3cf32421c9706917c3774025554decf4a
Paulo Luan <pauloluan.inova@gmail.com>
2020-09-07 10:05:51 -0300

    🎨 style: aligned items

--- react/general/src/Header/header.styles.ts
+++ react/general/src/Header/header.styles.ts
@@ -2,15 +2,20 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   display: flex;
+  place-items: center;
+  justify-content: space-between;
   background: var(--primary);
   padding: 20px 0;
 `
 
-export const Logo = styled.img``
+export const Logo = styled.img`
+  flex-shrink: 0;
+`
 
 export const Nav = styled.nav`
   > ul {
     > li {
+      display: inline-block;
       color: white;
     }
   }
