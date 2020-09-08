099fe71766b27cfcb9c8cea05a70dea2178f75b6
Paulo Luan <pauloluan.inova@gmail.com>
2020-09-08 04:57:08 -0300

    🎨 style: added paragraph styles

--- react/general/src/About/about.styles.ts
+++ react/general/src/About/about.styles.ts
@@ -8,4 +8,11 @@ export const AboutItem = styled.div``
 
 export const AboutImage = styled.img``
 
-export const Description = styled.p``
+export const Description = styled.p`
+  margin: 20px auto;
+  max-width: 460px;
+  text-align: center;
+  font-style: italic;
+  font-size: 1.125em;
+  line-height: 1.4em;
+`
