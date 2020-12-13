b43ab0c22b58a9709229758dde38ed5c16446657
Paulo Luan <pauloluan.inova@gmail.com>
2020-12-13 05:29:03 -0300

    📦 add: added support for material design

--- vue/monorepo/apps/frontend/src/main.ts
+++ vue/monorepo/apps/frontend/src/main.ts
@@ -1,9 +1,12 @@
 import Vue from 'vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+Vue.use(VueMaterial)
 
 new Vue({
   router,
