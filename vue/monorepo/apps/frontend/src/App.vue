bf66080b76fe85de505a86434b5bdba81f3ec413
Paulo Luan <pauloluan.inova@gmail.com>
2020-12-13 05:46:03 -0300

    ✨ feat: added Header with material design!

--- vue/monorepo/apps/frontend/src/App.vue
+++ vue/monorepo/apps/frontend/src/App.vue
@@ -1,32 +1,18 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <div class="home">
+    <Header />
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import Header from '@/components/Header.vue' // @ is an alias to /src
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+@Component({
+  components: {
+    Header
   }
-}
-</style>
+})
+export default class App extends Vue {}
+</script>
