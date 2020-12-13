9ba0e2dec2f546a100311616adf892a8f94318f8
Paulo Luan <pauloluan.inova@gmail.com>
2020-12-13 07:50:17 -0300

    added encode function in order to change the input string to a value that the machine learning algorithm can undestand

--- machine-learning/lula_ou_bolsonaro/libs/brain-machine/src/Train.ts
+++ machine-learning/lula_ou_bolsonaro/libs/brain-machine/src/Train.ts
@@ -8,7 +8,12 @@ class Train {
     _net = new brain.NeuralNetwork()
   }
 
-  getAllQuotes () {
+  execute () {
+    const { bolsonaro, lula } = this._getAllQuotes()
+    this._trainData([...bolsonaro, ...lula])
+  }
+
+  _getAllQuotes () {
     const bolsonaro = DataLake.Bolsonaro.data.map(({ quote }) =>
       this._createInputAndOutputForQuote(quote, 'bolsonaro')
     )
@@ -20,8 +25,15 @@ class Train {
     return { bolsonaro, lula }
   }
 
+  /**
+   So if we want out neural network to accept tweets (i.e. strings) as an input, we’ll need to run them through an similar function (called encode() below) that will turn every character in a string into a value between 0 and 1 and store it in an array. Fortunately, Javascript has a native method for converting any character into ASCII code called charCodeAt(). So we’ll use that and divide the outcome by the max value for Extended ASCII characters: 255 (we’re using extended ASCII just in case we encounter any fringe cases like é or ½) which will ensure that we get a value <1.
+   */
+  _encode (input) {
+    return input.split('').map(str => (str.charCodeAt(0) / 256))
+  }
+
   _createInputAndOutputForQuote (quote, author) {
-    return { input: quote, output: { [author]: 1 } }
+    return { input: this._encode(quote), output: { [author]: 1 } }
   }
 
   _trainData (data) {
