7c1ef494688719aecfc051b7292fb350568de251
Paulo Luan <pauloluan.inova@gmail.com>
2021-05-23 04:24:08 -0300

    🔧 chore: reformat code

--- css/css-training/apps/css-base/src/app/app.tsx
+++ css/css-training/apps/css-base/src/app/app.tsx
@@ -1,7 +1,7 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
-import { ReactComponent as Logo } from './logo.svg';
-import star from './star.svg';
+import { ReactComponent as Logo } from './logo.svg'
+import star from './star.svg'
 
 const StyledApp = styled.div`
   font-family: sans-serif;
@@ -130,7 +130,7 @@ const StyledApp = styled.div`
     width: 16px;
     margin-right: 4px;
   }
-`;
+`
 
 export function App() {
   return (
@@ -224,7 +224,7 @@ nx affected:e2e
         </details>
       </main>
     </StyledApp>
-  );
+  )
 }
 
-export default App;
+export default App
