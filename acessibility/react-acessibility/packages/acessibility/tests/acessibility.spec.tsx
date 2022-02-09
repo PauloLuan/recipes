d82cbb7569d77655f689785212d2b86bd872b9e1
Paulo Luan <pauloluan.inova@gmail.com>
2022-02-09 11:00:52 -0300

    ✅ test: added jest axe acessibility checker

--- acessibility/react-acessibility/packages/acessibility/tests/acessibility.spec.tsx
+++ acessibility/react-acessibility/packages/acessibility/tests/acessibility.spec.tsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import { screen } from '@testing-library/react'
 import { renderWithTheme } from '@pauloluan/shared'
-
+import { screen } from '@testing-library/react'
+import React from 'react'
 import { Acessibility } from '../src/acessibility.component'
+
+import { axe, toHaveNoViolations } from 'jest-axe'
+expect.extend(toHaveNoViolations)
+
 // import theme from '../../styles/Theme'
 
 describe('<Acessibility />', () => {
   it('should render the Acessibility component', () => {
     renderWithTheme(<Acessibility />)
     expect(screen.getByTestId('acessibility-component-id')).toBeInTheDocument()
+  })
+})
+
+describe('[AXE] Acessibility: <Acessibility />', () => {
+  it('Should not have ARIA violations', async () => {
+    const { container } = renderWithTheme(<Acessibility />)
+    const results = await axe(container)
+    expect(results).toHaveNoViolations()
   })
 })
