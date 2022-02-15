f715004aec60e8636b3ac39493774915d106b84e
Paulo Luan <pauloluan.inova@gmail.com>
2022-02-15 11:03:39 -0300

    �� fix: fixed wrong style

--- react_native/gameplay/src/components/button/button.styles.ts
+++ react_native/gameplay/src/components/button/button.styles.ts
@@ -2,5 +2,5 @@ import styled from 'styled-components/native'
 
 export const Button = styled.Button`
   flex: 1;
-  background-color: ${({ theme }) => theme.colors.background};
+  background-color: ${({ theme }) => theme.colors.primary};
 `
